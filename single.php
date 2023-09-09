3a2799362a7ae11a77e98f4867eb57ee16bc650e
Robert <robert-870519@hotmail.com>
2023-09-08 23:16:36 -0500

    Mostrando comentarios del blog y aplicandoles estilos CSS al formulario de comentarios para usuarios

--- single.php
+++ single.php
@@ -1,8 +1,38 @@
+<?php 
+/**
+ * 
+ * @link https://developer.wordpress.org/reference/functions/comment_form/
+ * @link https://developer.wordpress.org/reference/functions/get_template_part/
+ * @link https://developer.wordpress.org/reference/functions/wp_list_comments/
+ * @link https://developer.wordpress.org/reference/functions/get_comments/
+ * 
+ */
+
+?>
 <?php get_header(); ?>
 	<main class="container section-page">
 	 	<?php 
 	        get_template_part('template-parts/post');
 	 	 ?>
+	 	 <div class="comments">
+	 	 	<?php comment_form(); ?>
+	 	 	<h3 class="text-center text-primary"><?php echo  esc_html_e( "Comments", 'gymfitness' ); ?></h3>
+	 	 	<ul class="list-comments">
+	 	 		<?php 
+	 	 		//Obtemnemos los comentarios del blog
+                   $comments = get_comments(array(
+                      'post_id' => $post->ID,
+                      'status'  => 'approve'
+                   ));
+
+                   wp_list_comments(array(
+                       'per_page' => 10,
+                       'reverse_top_level' => false
+                   ), $comments);
+
+	 	 		?>
+	 	 	</ul>
+	 	 </div>
 	</main>
 
 <?php get_footer(); ?>
